3b7fbec5de929f51b8f238dccadbee042f5cf416
Franciele Trindade <francieletrindade78@gmail.com>
2020-02-18 17:10:32 -0300

    Exercício 01 e 02

---
+++ exercicio01.py
@@ -0,0 +1,5 @@
+nome=input("nome: ")
+mensagem = " Seja Bem Vinda!"
+mensagem2 = " Fico feliz em te ver!"
+print( mensagem, nome, mensagem2 )
+
---
+++ exercicio02.py
@@ -0,0 +1,7 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/55329/Documents/algoritimos/Algoritmo-python/exercicio 1.py
+nome: Jerusa
+ Seja Bem Vinda! Jerusa  Fico feliz em te ver!
+>>> 
