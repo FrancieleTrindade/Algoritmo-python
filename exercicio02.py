c65514c46bef9dd5d2b0034bd7db13860ccd9d4f
Franciele Trindade <francieletrindade78@gmail.com>
2020-02-18 18:26:41 -0300

    Substuição do arquivo02

--- exercicio02.py
+++ exercicio02.py
@@ -1,9 +1,11 @@
 Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-= RESTART: C:/Users/55329/Documents/algoritimos/Algoritmo-python/exercicio02.py
-Digite o dia: 25
-Digite o mês: 08
-Digite o ano: 2001
-25 / 08 / 2001
+>>> dia = input ("Digite o dia: ")
+Digite o dia: 20
+>>> mes = input ("Digite o mes: ")
+Digite o mes: 10
+>>> ano = input ("Digite ano: ")
+Digite ano: 2021
+>>> print (dia, "/", mes, "/", ano)
+20 / 10 / 2021
 >>> 
