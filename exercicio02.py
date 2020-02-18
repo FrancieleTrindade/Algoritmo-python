42be54b7aeeab871dd2e2f3688a2f651d7bef95b
Franciele Trindade <francieletrindade78@gmail.com>
2020-02-18 18:02:04 -0300

    Exercicio02

---
+++ exercicio02.py
@@ -0,0 +1,9 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/55329/Documents/algoritimos/Algoritmo-python/exercicio02.py
+Digite o dia: 25
+Digite o mês: 08
+Digite o ano: 2001
+25 / 08 / 2001
+>>> 
