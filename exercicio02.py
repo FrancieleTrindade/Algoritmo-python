18347551603be80f32cb12bf3c4bcb22b8d62bea
Franciele Trindade <francieletrindade78@gmail.com>
2020-02-18 18:33:44 -0300

    Código apresentado de forma mais legível

--- exercicio02.py
+++ exercicio02.py
@@ -1,11 +1,5 @@
-Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> dia = input ("Digite o dia: ")
-Digite o dia: 20
->>> mes = input ("Digite o mes: ")
-Digite o mes: 10
->>> ano = input ("Digite ano: ")
-Digite ano: 2021
->>> print (dia, "/", mes, "/", ano)
-20 / 10 / 2021
->>> 
+dia = input ("Digite o dia: ")
+mes = input ("Digite o mes: ")
+ano = input ("Digite o ano: ")
+
+print (dia, "/", mes, "/", ano)
